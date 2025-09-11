b53d5ffb68784482ae8749dd709593fe5eb7a87c
GitHub Action <action@github.com>
2025-09-11 08:25:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/sorium/filetaskmanager/MyBundle.kt
+++ src/main/kotlin/com/github/sorium/filetaskmanager/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.sorium.filetaskmanager
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/sorium/filetaskmanager/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/sorium/filetaskmanager/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.sorium.filetaskmanager.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
